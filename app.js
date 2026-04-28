2b43b8707639c2905cef2201e93c1f48e91f0e2f
Prashan5903s <chaubeyprashant498@gmail.com>
2026-04-28 16:34:48 +0530

    Changes of spark grid cron  email

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use((error, req, res, next) => {
     });
 });
 
-cron.schedule("*/10 10-18 * * 1-6", async () => {
+cron.schedule("*/4 10-18 * * 1-6", async () => {
 
     console.log("Running email cron...");
 
