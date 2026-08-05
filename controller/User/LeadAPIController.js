1baf0c71db449002242e01163a35de40620d0f61
Prashan5903s <chaubeyprashant498@gmail.com>
2026-08-05 12:29:26 +0530

    Changes of leads

--- controller/User/LeadAPIController.js
+++ controller/User/LeadAPIController.js
@@ -9,13 +9,247 @@ const AppConfig = require('../../model/AppConfig')
 
 const { successResponse, errorResponse } = require('../../util/response')
 
+const getLeadConfigLookupStages = () => [
+  {
+    $lookup: {
+      from: 'app_config',
+      let: {
+        statusId: '$lead_status_id',
+        sourceId: '$source_id',
+        solutionId: '$solution_id'
+      },
+      pipeline: [
+        {
+          $match: {
+            type: 'leads_data'
+          }
+        },
+        {
+          $project: {
+            leads_status_data: 1,
+            leads_source_data: 1,
+            solution_data: 1
+          }
+        },
+        {
+          $addFields: {
+            status: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$leads_status_data',
+                    as: 'item',
+                    cond: { $eq: ['$$item._id', '$$statusId'] }
+                  }
+                },
+                0
+              ]
+            },
+            source: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$leads_source_data',
+                    as: 'item',
+                    cond: { $eq: ['$$item._id', '$$sourceId'] }
+                  }
+                },
+                0
+              ]
+            },
+            solution: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$solution_data',
+                    as: 'item',
+                    cond: { $eq: ['$$item._id', '$$solutionId'] }
+                  }
+                },
+                0
+              ]
+            }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            status: 1,
+            source: 1,
+            solution: 1
+          }
+        }
+      ],
+      as: 'configData'
+    }
+  },
+  {
+    $addFields: {
+      lead_status: {
+        status: { $arrayElemAt: ['$configData.status', 0] }
+      },
+      source: { $arrayElemAt: ['$configData.source', 0] },
+      solution: { $arrayElemAt: ['$configData.solution', 0] }
+    }
+  },
+  {
+    $project: {
+      configData: 0
+    }
+  }
+]
+
+// Reusable pipeline stages for populating follow-ups and their configurations
+const getFollowUpLookupStages = () => [
+  {
+    $lookup: {
+      from: 'follow_up',
+      let: { leadId: '$_id' },
+      pipeline: [
+        {
+          $match: {
+            $expr: { $eq: ['$lead_id', '$$leadId'] }
+          }
+        },
+        {
+          $sort: { created_at: -1 }
+        }
+      ],
+      as: 'followUp'
+    }
+  },
+  {
+    $unwind: {
+      path: '$followUp',
+      preserveNullAndEmptyArrays: true
+    }
+  },
+  {
+    $addFields: {
+      followUp: {
+        $cond: [{ $ifNull: ['$followUp._id', false] }, '$followUp', null]
+      }
+    }
+  },
+  {
+    $lookup: {
+      from: 'app_config',
+      let: {
+        statusId: '$followUp.status',
+        priorityId: '$followUp.priority',
+        typeId: '$followUp.follow_up_type'
+      },
+      pipeline: [
+        {
+          $match: { type: 'follow_up_data' }
+        },
+        {
+          $project: {
+            follow_up_status_data: 1,
+            follow_up_priority_data: 1,
+            follow_up_type_data: 1
+          }
+        },
+        {
+          $addFields: {
+            status: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$follow_up_status_data',
+                    as: 'item',
+                    cond: { $eq: ['$$item._id', '$$statusId'] }
+                  }
+                },
+                0
+              ]
+            },
+            priority: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$follow_up_priority_data',
+                    as: 'item',
+                    cond: { $eq: ['$$item._id', '$$priorityId'] }
+                  }
+                },
+                0
+              ]
+            },
+            follow_up_type: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$follow_up_type_data',
+                    as: 'item',
+                    cond: { $eq: ['$$item._id', '$$typeId'] }
+                  }
+                },
+                0
+              ]
+            }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            status: 1,
+            priority: 1,
+            follow_up_type: 1
+          }
+        }
+      ],
+      as: 'followUpConfig'
+    }
+  },
+  {
+    $addFields: {
+      'followUp.status_data': { $arrayElemAt: ['$followUpConfig.status', 0] },
+      'followUp.priority_data': {
+        $arrayElemAt: ['$followUpConfig.priority', 0]
+      },
+      'followUp.type_data': {
+        $arrayElemAt: ['$followUpConfig.follow_up_type', 0]
+      }
+    }
+  },
+  {
+    $project: {
+      followUpConfig: 0
+    }
+  },
+  {
+    $group: {
+      _id: '$_id',
+      doc: { $first: '$$ROOT' },
+      followUps: {
+        $push: {
+          $cond: [
+            { $ifNull: ['$followUp._id', false] },
+            '$followUp',
+            '$$REMOVE'
+          ]
+        }
+      }
+    }
+  },
+  {
+    $addFields: {
+      'doc.followUp': { $ifNull: ['$followUps', []] }
+    }
+  },
+  {
+    $replaceRoot: { newRoot: '$doc' }
+  }
+]
+
 exports.getLeadAPIController = async (req, res, next) => {
   try {
     const userId = req?.userId
-
     const { type } = req?.params
-
     let finalData = []
+
+    const userObjectId = mongoose.Types.ObjectId.createFromHexString(userId)
 
     if (type === 'kanban') {
       const data = await AppConfig.aggregate([
@@ -32,186 +266,21 @@ exports.getLeadAPIController = async (req, res, next) => {
             from: 'leads',
             let: {
               statusId: '$leads_status_data._id',
-              userId: mongoose.Types.ObjectId.createFromHexString(userId)
+              userId: userObjectId
             },
             pipeline: [
               {
                 $match: {
                   $expr: {
                     $and: [
-                      {
-                        $eq: ['$lead_status_id', '$$statusId']
-                      },
-                      {
-                        $eq: ['$created_by', '$$userId']
-                      } // ✅ FILTER HERE
+                      { $eq: ['$lead_status_id', '$$statusId'] },
+                      { $eq: ['$created_by', '$$userId'] }
                     ]
                   }
                 }
               },
-              {
-                $lookup: {
-                  from: 'follow_up',
-                  let: {
-                    leadId: '$_id'
-                  },
-                  pipeline: [
-                    {
-                      $match: {
-                        $expr: {
-                          $eq: ['$lead_id', '$$leadId']
-                        }
-                      }
-                    },
-                    {
-                      $sort: {
-                        created_at: -1
-                      }
-                    }
-                  ],
-                  as: 'followUp'
-                }
-              },
-              {
-                $unwind: {
-                  path: '$followUp',
-                  preserveNullAndEmptyArrays: true
-                }
-              },
-              {
-                $addFields: {
-                  followUp: {
-                    $cond: [
-                      {
-                        $ifNull: ['$followUp._id', false]
-                      },
-                      '$followUp',
-                      null
-                    ]
-                  }
-                }
-              },
-              {
-                $lookup: {
-                  from: 'app_config',
-                  let: {
-                    statusId: '$followUp.status',
-                    priorityId: '$followUp.priority',
-                    typeId: '$followUp.follow_up_type'
-                  },
-                  pipeline: [
-                    {
-                      $match: {
-                        type: 'follow_up_data'
-                      }
-                    },
-                    {
-                      $addFields: {
-                        status: {
-                          $arrayElemAt: [
-                            {
-                              $filter: {
-                                input: '$follow_up_status_data',
-                                as: 'item',
-                                cond: {
-                                  $eq: ['$$item._id', '$$statusId']
-                                }
-                              }
-                            },
-                            0
-                          ]
-                        },
-                        priority: {
-                          $arrayElemAt: [
-                            {
-                              $filter: {
-                                input: '$follow_up_priority_data',
-                                as: 'item',
-                                cond: {
-                                  $eq: ['$$item._id', '$$priorityId']
-                                }
-                              }
-                            },
-                            0
-                          ]
-                        },
-                        follow_up_type: {
-                          $arrayElemAt: [
-                            {
-                              $filter: {
-                                input: '$follow_up_type_data',
-                                as: 'item',
-                                cond: {
-                                  $eq: ['$$item._id', '$$typeId']
-                                }
-                              }
-                            },
-                            0
-                          ]
-                        }
-                      }
-                    },
-                    {
-                      $project: {
-                        _id: 0,
-                        status: 1,
-                        priority: 1,
-                        follow_up_type: 1
-                      }
-                    }
-                  ],
-                  as: 'followUpConfig'
-                }
-              },
-              {
-                $addFields: {
-                  'followUp.status_data': {
-                    $arrayElemAt: ['$followUpConfig.status', 0]
-                  },
-                  'followUp.priority_data': {
-                    $arrayElemAt: ['$followUpConfig.priority', 0]
-                  },
-                  'followUp.type_data': {
-                    $arrayElemAt: ['$followUpConfig.follow_up_type', 0]
-                  }
-                }
-              },
-              {
-                $project: {
-                  followUpConfig: 0
-                }
-              },
-              {
-                $group: {
-                  _id: '$_id',
-                  doc: {
-                    $first: '$$ROOT'
-                  },
-                  followUps: {
-                    $push: {
-                      $cond: [
-                        {
-                          $ifNull: ['$followUp._id', false]
-                        },
-                        '$followUp',
-                        '$$REMOVE'
-                      ]
-                    }
-                  }
-                }
-              },
-              {
-                $addFields: {
-                  'doc.followUp': {
-                    $ifNull: ['$followUps', []]
-                  }
-                }
-              },
-              {
-                $replaceRoot: {
-                  newRoot: '$doc'
-                }
-              }
+              ...getFollowUpLookupStages(),
+              ...getLeadConfigLookupStages()
             ],
             as: 'leads'
           }
@@ -230,218 +299,21 @@ exports.getLeadAPIController = async (req, res, next) => {
       const leads = await Lead.aggregate([
         {
           $match: {
-            created_by: mongoose.Types.ObjectId.createFromHexString(userId)
+            created_by: userObjectId
           }
         },
-
-        // 1. Get follow ups
-        {
-          $lookup: {
-            from: 'follow_up',
-            let: {
-              leadId: '$_id'
-            },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                    $eq: ['$lead_id', '$$leadId']
-                  }
-                }
-              },
-              {
-                $sort: {
-                  created_at: -1
-                } // descending order
-              }
-            ],
-            as: 'followUp'
-          }
-        },
-
-        // 2. Unwind followUp
-        {
-          $unwind: {
-            path: '$followUp',
-            preserveNullAndEmptyArrays: true
-          }
-        },
-
-        // 3. Normalize empty followUp → null
-        {
-          $addFields: {
-            followUp: {
-              $cond: [
-                {
-                  $ifNull: ['$followUp._id', false]
-                },
-                '$followUp',
-                null
-              ]
-            }
-          }
-        },
-
-        // 4. Lookup app_config for followUp mappings
+        ...getFollowUpLookupStages(),
+        ...getLeadConfigLookupStages(),
         {
           $lookup: {
             from: 'app_config',
-            let: {
-              statusId: '$followUp.status',
-              priorityId: '$followUp.priority',
-              typeId: '$followUp.follow_up_type'
-            },
+            let: { statusId: '$lead_status_id' },
             pipeline: [
+              { $match: { type: 'leads_data' } },
+              { $unwind: '$leads_status_data' },
               {
                 $match: {
-                  type: 'follow_up_data'
-                }
-              },
-              {
-                $project: {
-                  follow_up_status_data: 1,
-                  follow_up_priority_data: 1,
-                  follow_up_type_data: 1
-                }
-              },
-              {
-                $addFields: {
-                  status: {
-                    $arrayElemAt: [
-                      {
-                        $filter: {
-                          input: '$follow_up_status_data',
-                          as: 'item',
-                          cond: {
-                            $eq: ['$$item._id', '$$statusId']
-                          }
-                        }
-                      },
-                      0
-                    ]
-                  },
-                  priority: {
-                    $arrayElemAt: [
-                      {
-                        $filter: {
-                          input: '$follow_up_priority_data',
-                          as: 'item',
-                          cond: {
-                            $eq: ['$$item._id', '$$priorityId']
-                          }
-                        }
-                      },
-                      0
-                    ]
-                  },
-                  follow_up_type: {
-                    $arrayElemAt: [
-                      {
-                        $filter: {
-                          input: '$follow_up_type_data',
-                          as: 'item',
-                          cond: {
-                            $eq: ['$$item._id', '$$typeId']
-                          }
-                        }
-                      },
-                      0
-                    ]
-                  }
-                }
-              },
-              {
-                $project: {
-                  _id: 0,
-                  status: 1,
-                  priority: 1,
-                  follow_up_type: 1
-                }
-              }
-            ],
-            as: 'followUpConfig'
-          }
-        },
-
-        // 5. Merge config into followUp
-        {
-          $addFields: {
-            'followUp.status_data': {
-              $arrayElemAt: ['$followUpConfig.status', 0]
-            },
-            'followUp.priority_data': {
-              $arrayElemAt: ['$followUpConfig.priority', 0]
-            },
-            'followUp.type_data': {
-              $arrayElemAt: ['$followUpConfig.follow_up_type', 0]
-            }
-          }
-        },
-
-        // 6. Remove temp field
-        {
-          $project: {
-            followUpConfig: 0
-          }
-        },
-
-        // 7. Regroup followUps safely (NO empty objects)
-        {
-          $group: {
-            _id: '$_id',
-            doc: {
-              $first: '$$ROOT'
-            },
-            followUps: {
-              $push: {
-                $cond: [
-                  {
-                    $ifNull: ['$followUp._id', false]
-                  },
-                  '$followUp',
-                  '$$REMOVE'
-                ]
-              }
-            }
-          }
-        },
-
-        // 8. Ensure empty array if none
-        {
-          $addFields: {
-            'doc.followUp': {
-              $ifNull: ['$followUps', []]
-            }
-          }
-        },
-
-        {
-          $replaceRoot: {
-            newRoot: '$doc'
-          }
-        },
-
-        // 9. Lead status lookup (your existing logic)
-        {
-          $lookup: {
-            from: 'app_config',
-            let: {
-              statusId: '$lead_status_id'
-            },
-            pipeline: [
-              {
-                $match: {
-                  type: 'leads_data'
-                }
-              },
-              {
-                $unwind: '$leads_status_data'
-              },
-              {
-                $match: {
-                  $expr: {
-                    $eq: ['$leads_status_data._id', '$$statusId']
-                  }
+                  $expr: { $eq: ['$leads_status_data._id', '$$statusId'] }
                 }
               },
               {
@@ -454,7 +326,6 @@ exports.getLeadAPIController = async (req, res, next) => {
             as: 'lead_status'
           }
         },
-
         {
           $unwind: {
             path: '$lead_status',
@@ -495,6 +366,7 @@ exports.getCreateLeadAPIController = async (req, res, next) => {
     const appConfig = await AppConfig.findOne({
       type: 'leads_data'
     })
+
     const sourceData = appConfig?.leads_source_data || []
     const statusData = appConfig?.leads_status_data || []
     const solutionData = appConfig?.solution_data || []
